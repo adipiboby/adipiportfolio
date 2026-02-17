8258f5c8789307664e489bd4e41e7117050bf2f8
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-02-17 12:21:05 +0530

    first commit

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
-import "../src\\components\\Contact.css"
+import React, { useState } from "react";
+import "../src/components/Contact.css";
 export default function Contact() {
-const [email,setEmail]=useState("")
-    function emailfun(e){
-console.log("hii")
-    }
+  const [email, setEmail] = useState("");
+  function emailfun(e) {
+    console.log("hii");
+  }
   return (
     <div className="Contactpage">
       <h1 id="contactheading">Contact</h1>
       <h3 id="contactintro">Feel free to reach out to me anything!</h3>
-      
+
       <div className="contactdiv">
         <h1 id="emailme">Email Me 🚀</h1>
-        
-<form id="inputdiv">
-        <input placeholder='Your Email' value={email} type="text" onChange={emailfun} id="email"></input>
-        <input placeholder='Your Name' id='contactName'></input>
-        <input placeholder='Message' id="message"></input>
-        <button>Send</button>
-       </form>
+
+        <form id="inputdiv">
+          <input
+            placeholder="Your Email"
+            value={email}
+            type="text"
+            onChange={emailfun}
+            id="email"
+          ></input>
+          <input placeholder="Your Name" id="contactName"></input>
+          <input placeholder="Message" id="message"></input>
+          <button>Send</button>
+        </form>
       </div>
-     
     </div>
-  )
+  );
 }
